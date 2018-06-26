55814845480a4e9f424bd38d478ab5b4c4d4bc83
admin <jlin@cloudera.com>
2018-06-26 21:24:04 +0000

    add missing space to avoid error message during build

--- cdsw-build.sh
+++ cdsw-build.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
 
 #!/bin/bash
 
 if [ -d "models/spark/" ] 
 then
   rm -r -f models/spark/*
-  if [ -f "models/spark_rf.tar"]
+  if [ -f "models/spark_rf.tar" ]
   then tar -xf models/spark_rf.tar 
   fi
 fi
